ef1b6a4b390ffa72ac95bcb8836e2253ea1ba0ea
rizkihutama <rizkihutama05@gmail.com>
2023-03-12 12:16:05 +0700

    adding basic feedback validation input

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
     const nameInputRef = useRef();
     const [enteredName, setEnteredName] = useState('');
+    const [isNameInputValid, setIsNameInputValid] = useState(true);
 
     const nameInputChangeHandler = event => {
         setEnteredName(event.target.value);
@@ -11,6 +12,12 @@ const SimpleInput = (props) => {
     const formSubmitHandler = event => {
         event.preventDefault();
 
+        if (enteredName.trim() === '') {
+            setIsNameInputValid(false);
+            return;
+        } 
+        
+        setIsNameInputValid(true);
         console.log(`${enteredName} -> value from state`);
 
         const inputNameValue = nameInputRef.current.value;
@@ -21,11 +28,14 @@ const SimpleInput = (props) => {
         setEnteredName('');
     };
 
+    const inputNameClasses = isNameInputValid ? 'form-control' : 'form-control invalid';
+
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className='form-control'>
+            <div className={inputNameClasses}>
                 <label htmlFor='name'>Your Name</label>
                 <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
+                {!isNameInputValid && <p className="error-text">Name is Required</p>}
             </div>
             <div className="form-actions">
                 <button>Submit</button>
